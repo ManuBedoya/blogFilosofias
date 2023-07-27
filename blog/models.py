c674c77677ce3366dd643d7bf311b86ec48ee527
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-07-27 10:06:59 -0500

    update

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
-    title=models.CharField(max_length=250)
-    content=models.TextField(default="Some")
+    title = models.CharField(max_length=250)
+    content = models.TextField(default="Some")
 
     def __str__(self):
         return self.title
+
+    def getDetail(self):
+        return f'Title: {self.title}\nContent: {self.content}'
